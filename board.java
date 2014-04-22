68269a579f39b6c84a597a41b671d37292a433f2
Cox <truelonghornfan@comcast.net>
2014-04-22 08:56:57 -0600

    Cleaned up the board code

--- board.java
+++ board.java
@@ -1,6 +1,8 @@
-<<<<<<< HEAD
-public class Board{
+import java.applet.*;
+import java.awt.*;
+import java.util.*;
 
+public class board extends Applet{
 	public board(){
 		int[][] board = {{16,15,14,13,12,14,15,16}
 						 {11,11,11,11,11,11,11,11}
@@ -14,47 +16,37 @@ public class Board{
 		
 	}
 
-}
-=======
-	import java.applet.*;
-	import java.awt.*;
-	import java.util.*;
+	public void drawBoard(){
+		paint(g);
+	}
 
-	public class board extends Applet
-	{
-		public void drawBoard() {
-			paint(g);
-		}
-
-	    public void paint(Graphics g) {
-	        int w=getWidth();
-	        int h=getHeight();
-	        int y=0;
-	        for(int j=1;j<=4;j++) {
+	public void paint(Graphics g) {
+	    int w=getWidth();
+	    int h=getHeight();
+	    int y=0;
+	    for(int j=1;j<=4;j++) {
 
 
-	        for(int i=0;i<=w;i=i+w/8) {
+	    	for(int i=0;i<=w;i=i+w/8) {
 
+	        	g.setColor(Color.BLACK);
+	        	g.fillRect(i,y,w/8,h/8);
+	        	i=i+w/8;
+	        	g.setColor(Color.WHITE);
+	        	g.fillRect(i,y,w/8,h/8);
 
-
-	            g.setColor(Color.BLACK);
-	            g.fillRect(i,y,w/8,h/8);
-	            i=i+w/8;
-	            g.setColor(Color.WHITE);
-	            g.fillRect(i,y,w/8,h/8);
-
-	        }
-	            y=y+h/8;
-	        for( int i=0;i<=w;i=i+w/8) {
-	            g.setColor(Color.WHITE);
-	            g.fillRect(i,y,w/8,h/8);
-	            i=i+w/8;
-	            g.setColor(Color.BLACK);
-	            g.fillRect(i,y,w/8,h/8);
-	        }
-	            y=y+h/8;
+	    	}
+	       	y=y+h/8;
+	    	for( int i=0;i<=w;i=i+w/8) {
+	        	g.setColor(Color.WHITE);
+	        	g.fillRect(i,y,w/8,h/8);
+	        	i=i+w/8;
+	        	g.setColor(Color.BLACK);
+	        	g.fillRect(i,y,w/8,h/8);
+	    	}
+	        y=y+h/8;
 	    }
 
-	     }
 	}
->>>>>>> 33965aa07cbf89456fb9768c8329d42d01a6b667
+}
+
